039e89fda11fb500f3fcf82980fd1a44b7f4dc60
BuildTools <unconfigured@null.spigotmc.org>
2022-05-12 17:06:10 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunnyGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
