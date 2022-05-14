267909c611ea855a63a8c6e583e2853e984496d8
BuildTools <unconfigured@null.spigotmc.org>
2022-05-14 15:00:07 -0700

    Latest version

--- Password.cs
+++ Password.cs
@@ -9,6 +9,7 @@ namespace FunnyGame
     {
 
         private string enterPassword;
+        private string enterPasswordMelon;
         private string password = "OC_25";
         private string passLow = "oc_25";
 
@@ -38,11 +39,30 @@ namespace FunnyGame
             else if (enterPassword == "OVERRIDE")
             {
                 Console.Clear();
+                Console.WriteLine("Hello *MELON*, please enter your password");
+                goto melonPass;
             }
             else
             {
                 Console.WriteLine("ERROR CANNOT FIND PROJECT SPECIFIED, PLEASE TYPE IN CORRECT PROJECT ID");
                 goto enter;
+            }
+
+        melonPass:
+
+            enterPasswordMelon = Console.ReadLine();
+
+            if (enterPasswordMelon == "ATM")
+            {
+                Console.WriteLine("Correct Password, unfortunately, this section is still under construction, please come back soon");
+                Thread.Sleep(2500);
+                Console.Clear();
+                goto enter;
+            } else
+            {
+                Console.WriteLine("INCORRECT PASSWORD, REBOOTING");
+                Thread.Sleep(2000);
+                Environment.Exit(0);
             }
         }
     }
